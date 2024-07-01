41684c8c9c568cf8f3ea9e7298c9703fdc7f89e3
John Rey <adioskid@mail.ru>
2024-07-01 23:16:33 +0400

    Create Train.java

--- src/main/java/com/app/transport/Train.java
+++
@@ -1,8 +0,0 @@
-package com.app.transport;
-
-public class Train extends Transport{
-    public Train(String brand, String model, int year, String country, String color, int maxSpeed) {
-        super(brand, model, year, country, color, maxSpeed);
-    }
-
-}
